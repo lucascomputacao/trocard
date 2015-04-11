b5220a11cbee3f4030a7372de674cef711a2107f
Lucas Borges <lucascomputacao@gmail.com>
2015-04-10 21:18:53 -0300

    adicionando arquivos do netbeans

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=trocard
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/trocard
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=trocard.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/trocard.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=trocard
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/trocard
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=trocard.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/trocard.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
