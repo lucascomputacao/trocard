c96b90bf685d916d1cee651747bdc9ec97c67ce2
Lucas Borges <lucascomputacao@gmail.com>
2015-04-10 21:18:21 -0300

    adicionando arquivo principal do projeto

---
+++ trocard.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Sammy Guergachi <sguergachi at gmail.com>
+ *
+ * Created on 10 de Abril de 2015, 21:17
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
