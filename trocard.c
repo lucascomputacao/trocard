0d3e4da45f2faa5c4e601c2f7288717381ff7285
Lucas Borges <lucascomputacao@gmail.com>
2015-04-22 18:16:05 -0300

    Backup de código - antes de alterações

--- trocard.c
+++ trocard.c
@@ -7,23 +7,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define MAXVETOR 9999
+#define MAXVETOR 99999  
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    long int array[MAXVETOR];
-    long int i,compare, count;
+    long int alice[MAXVETOR];
+    long int i, compare, count;
     char vetchar[MAXVETOR];
     int alice, beatriz;
+
+/*
+    for (i = 0; i < MAXVETOR; i++) {
+        alice[i] = i;
+        vetchar[i] = '\0';
+    }
+*/
+
     /*
-
-        for (i = 0; i < MAXVETOR; i++) {
-            array[i] = i;
-            vetchar[i] = '\0';
-        }
-
         printf("tamanho: %ld \n", sizeof (array));
         printf("tamanho char: %ld\n", sizeof (vetchar));
      */
@@ -35,21 +37,26 @@ int main(int argc, char** argv) {
         //segunda linha - cartas de Alice
         for (i = 0; i < alice; i++) {
             printf("\nSequencia de Alice\n");
-            scanf("%d", &vetchar[i]);
-            printf("lido:%d\t", vetchar[i]);
+            // scanf("%d", &vetchar[i]);
+            scanf("%ld", &alice[i]);
+            //printf("lido:%d\t", vetchar[i]);
+            printf("lido:%ld\t", alice[i]);
         }
-        if (vetchar[15] == NULL) {
-            printf("\n Nulo o teste");
-        }
-        printf("tamanho char: %ld\n", sizeof (vetchar));
+        /*
+                if (vetchar[15] == NULL) {
+                    printf("\n Nulo o teste\n");
+                }
+         */
+        //printf("tamanho char: %d\n", sizeof (vetchar));
+
         //terceira linha - cartas de Beatriz
         for (i = 0; i < beatriz; i++) {
             printf("\nSequencia de Beatriz\n");
             // função de hashing? teste para saber se há no vetor
             scanf("%d", compare);
             // teste para saber se temos a carta já armazenada no vetor
-            if (vetchar[compare] == NULL) {
-                //se der nulo, Alice não tem essa carta 
+            if (alice[compare] == NULL) {
+                //se der nulo, Alice não tem essa carta e a mesma pode ser trocada
                 count++;
             }
         }
